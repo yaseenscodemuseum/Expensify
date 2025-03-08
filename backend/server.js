ec2c2868d4479a715e685ea88602c712782812de
Yaseen <jade.chxnn@gmail.com>
2025-03-09 02:05:06 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: __dirname + "/.env" });
+require("dotenv").config(); // Automatically finds .env in the root
 
 const express = require("express");
 const mongoose = require("mongoose");
